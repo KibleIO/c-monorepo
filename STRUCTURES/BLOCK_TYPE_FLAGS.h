8786988388b6c9aec5b15832db1effc75e87ba81
j0ker <j0ker@desktop3.desktop3>
2018-04-13 13:35:40 -0700

    Alec

--- STRUCTURES/BLOCK_TYPE_FLAGS.h
+++
@@ -1,8 +0,0 @@
-#ifndef BLOCK_TYPE_FLAGS_H_
-#define BLOCK_TYPE_FLAGS_H_
-
-#define BLOCK_TYPE_GAIA_LOCAL                                                  1
-#define BLOCK_TYPE_GAIA_REMOTE                                                 2
-#define BLOCK_TYPE_GAIA_ROOT                                                   3
-
-#endif // BLOCK_TYPE_FLAGS_H_
--- STRUCTURES/HERMES/REMOTE_ACTOR.h
+++
@@ -1,4 +0,0 @@
-#include "REMOTE_ACTOR.h"
-
-// decrypts packet to remote_gaia command queue which decodes and applies it (see Decrypt_Hermes_Header_Actor)
-void Decrypt_Hermes_Header_Actor  (CONCURRENT_BLOCK* const calling_block, DATA_BLOCK* const encrypted_data);
