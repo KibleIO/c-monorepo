6f830a64023deda4f61de55c6000be3fc8de5c20
j0ker <ktb92677@yahoo.com>
2022-01-19 16:56:15 -0800

    somethings were named wrong

--- Rana_Core_Utils/Utilities/ASSERT.h
+++ Rana_Core_Utils/Utilities/ASSERT.h
@@ -4,29 +4,29 @@
 //okay, this has been needed for a while, assert
 
 //assert and print to console via cout
-#define ASSERT_P(bool, message) \
-if (!(bool)) {\
+#define ASSERT_P(boolean, message) \
+if (!(boolean)) {\
 	cout << message << endl;\
 }
 
 //assert and print to console via cout and return current function
-#define ASSERT_P_R(bool, message) \
-if (!(bool)) {\
+#define ASSERT_P_R(boolean, message) \
+if (!(boolean)) {\
 	cout << message << endl;\
 	return false;\
 }
 
 //assert and log to elk
-#define ASSERT_E(bool, message, ctx) \
-if (!(bool)) {\
+#define ASSERT_E(boolean, message, ctx) \
+if (!(boolean)) {\
 	LOG_ERROR_CTX((ctx)) {\
 		ADD_STR_LOG("message", message);\
 	}\
 }
 
 //assert and log to elk and return current function
-#define ASSERT_E_R(bool, message, ctx) \
-if (!(bool)) {\
+#define ASSERT_E_R(boolean, message, ctx) \
+if (!(boolean)) {\
 	LOG_ERROR_CTX((ctx)) {\
 		ADD_STR_LOG("message", message);\
 	}\
@@ -34,8 +34,8 @@ if (!(bool)) {\
 }
 
 //assert and log to elk and return current function
-#define ASSERT_E_B(bool, message, ctx) \
-if (!(bool)) {\
+#define ASSERT_E_B(boolean, message, ctx) \
+if (!(boolean)) {\
 	LOG_ERROR_CTX((ctx)) {\
 		ADD_STR_LOG("message", message);\
 	}\
